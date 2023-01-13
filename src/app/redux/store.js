6707b2a29938b4d7fa8173f2144f8ff06ad91707
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-13 19:46:24 +0700

    redux persist

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -3,12 +3,25 @@ import artistApi from "./api/artistApi";
 import playlistApi from "./api/playlistApi";
 import trackApi from "./api/trackApi";
 import rootReducer from "./rootReducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+const persistConfig = {
+	key: "root",
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-	reducer: rootReducer,
-	middleware: (getDefaultMiddleware) => {
-		return getDefaultMiddleware().concat([trackApi.middleware, artistApi.middleware, playlistApi.middleware]);
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>  getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}).concat([trackApi.middleware, artistApi.middleware, playlistApi.middleware]);
 	},
-});
+);
 
+export const persistor = persistStore(store);
 export default store;
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import store from "./app/redux/store";
+import store, { persistor } from "./app/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 import { Provider } from "react-redux";
 import AppProvider from "./components/context/AppProvider";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<Provider store={store}>
-		<AppProvider>
-			<App />
-		</AppProvider>
+		<PersistGate loading={null} persistor={persistor}>
+			<AppProvider>
+				<App />
+			</AppProvider>
+		</PersistGate>
 	</Provider>,
 );
