7b50a82091c10164a0dcb0fcb258c0f8181fed7b
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-14 18:42:36 +0700

    update

--- src/components/shared/Track/TrackList.jsx
+++ src/components/shared/Track/TrackList.jsx
@@ -8,14 +8,10 @@ import TrackCardSkeleton from "./TrackCardSkeleton";
 export const StyledTracksList = tw.table`table border-separate border-spacing-x-0 border-spacing-y-1 w-full`;
 
 const TrackList = () => {
-	const { data, isFetching, isError, isSuccess } = useFetchTracksQuery({ skip: 0, limit: 5 });
+	const { data } = useFetchTracksQuery({ skip: 0, limit: 5 });
 
 	return (
 		<StyledTracksList>
-			{isFetching &&
-				[(1, 2, 3, 4, 5)].map((item) => {
-					return <TrackCardSkeleton key={item} />;
-				})}
 			{Array.isArray(data) &&
 				data.map((track, index) => {
 					return <TrackCard key={track._id} className="rounded-xl" track={track} index={index + 1} />;
