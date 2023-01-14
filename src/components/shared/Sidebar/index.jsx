3d70523f73dfda24332bbe37185f1a1c40c162fc
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-14 18:23:03 +0700

    feat: loop + skip track + shuffle play

--- src/components/shared/Sidebar/index.jsx
+++ src/components/shared/Sidebar/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import tw from "tailwind-styled-components";
 import { Menu } from "../Atomics/Menu";
 import SidebarMenu from "./SidebarMenu";
@@ -8,12 +8,18 @@ const DrawerSidebar = tw.aside`drawer-side`;
 const DrawerOverlay = tw.label`drawer-overlay`;
 const DrawerSideWrapper = tw.div`p-2 bg-neutral w-fit`;
 const LogoImage = tw.img`max-w-[240px] h-[120px] object-contain -translate-x-2`;
-import { memo } from "react";
+import { memo, useEffect, useRef } from "react";
 
 const Sidebar = () => {
+	const overlayRef = useRef(null);
+	const location = useLocation();
+	useEffect(() => {
+		overlayRef.current.click();
+	}, [location]);
+
 	return (
 		<DrawerSidebar>
-			<DrawerOverlay htmlFor="sidebar-toggle" />
+			<DrawerOverlay htmlFor="sidebar-toggle" ref={overlayRef} />
 			<DrawerSideWrapper>
 				<Link to="/">
 					<LogoImage src={Logo} alt="" />
