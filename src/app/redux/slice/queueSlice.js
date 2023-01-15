54decaacd07d3c64982c1aa5c820dcf7c252d5b4
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-15 23:42:21 +0700

    queue redux slice

--- src/app/redux/slice/queueSlice.js
+++ src/app/redux/slice/queueSlice.js
@@ -1,16 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import instance from "@/app/axios/instance";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+const fetchRelatedTrackThunkAction = createAsyncThunk(
+	"queue/fetch_related_tracks",
+	async (genre, { rejectWithValue }) => {
+		try {
+			return await instance.get(`/track/related/${genre}`);
+		} catch (error) {
+			rejectWithValue([]);
+		}
+	},
+);
 const queueSlice = createSlice({
 	name: "queue",
-	initialState: [],
+	initialState: { currentTrack: null, nextup: [] },
 	reducers: {
+		setCurrentTrack: (state, action) => {
+			state.currentTrack = action.payload;
+		},
+		setCurrentPlaylist: (state, action) => {
+			state.nextup = action.payload;
+			state.currentTrack = [action.payload];
+		},
 		addToQueue: (state, action) => {
-			return [action.payload, ...state];
+			state.nextup = [action.payload, ...state.nextup];
 		},
 		removeFromQueue: (state, action) => {
 			return state.filter((track) => track._id !== action.payload._id);
 		},
 	},
+	extraReducers: (builder) => {
+		builder.addCase(fetchRelatedTrackThunkAction.fulfilled, (state, action) => {
+			state.nextup = action.payload;
+		});
+	},
 });
-export const { addToQueue, removeFromQueue } = queueSlice.actions;
+
+export { fetchRelatedTrackThunkAction };
+export const { addToQueue, removeFromQueue, setCurrentTrack, setCurrentPlaylist } = queueSlice.actions;
 export default queueSlice;
