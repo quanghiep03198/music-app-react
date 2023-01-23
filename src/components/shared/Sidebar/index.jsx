6ba554b6916d7df5c9e2fabf606be12bc2e237bf
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-24 01:39:51 +0700

    fix: close sidebar after change page

--- src/components/shared/Sidebar/index.jsx
+++ src/components/shared/Sidebar/index.jsx
@@ -1,28 +1,34 @@
-import { memo, useEffect, useRef } from "react";
-import { Link, useLocation } from "react-router-dom";
-import tw from "tailwind-styled-components";
-import SidebarMenu from "./SidebarMenu";
-import Logo from "/images/logo.png";
+import { memo, useEffect, useRef } from "react"
+import { Link, useLocation } from "react-router-dom"
+import tw from "tailwind-styled-components"
+import SidebarMenu from "./SidebarMenu"
+import Logo from "/images/logo.png"
 
-const DrawerSidebar = tw.aside`drawer-side`;
-const DrawerOverlay = tw.label`drawer-overlay`;
-const DrawerSideWrapper = tw.div`p-2 bg-base-300 w-fit`;
-const LogoImage = tw.img`max-w-[240px] h-[120px] object-contain -translate-x-2`;
+const DrawerSidebar = tw.aside`drawer-side`
+const DrawerOverlay = tw.label`drawer-overlay`
+const DrawerSideWrapper = tw.div`p-2 bg-base-300 w-fit`
+const LogoImage = tw.img`max-w-[240px] h-[120px] object-contain -translate-x-2`
 
 const Sidebar = () => {
-	return (
-		<DrawerSidebar>
-			<DrawerOverlay htmlFor="sidebar-toggle" />
-			<DrawerSideWrapper>
-				<Link to="/">
-					<LogoImage src={Logo} alt="" />
-				</Link>
+    const { pathname } = useLocation()
+    const overlayRef = useRef()
+    useEffect(() => {
+        overlayRef.current.click()
+    }, [pathname])
 
-				<SidebarMenu />
-				<div className="divider"></div>
-			</DrawerSideWrapper>
-		</DrawerSidebar>
-	);
-};
+    return (
+        <DrawerSidebar>
+            <DrawerOverlay htmlFor="sidebar-toggle" ref={overlayRef} />
+            <DrawerSideWrapper>
+                <Link to="/">
+                    <LogoImage src={Logo} alt="" />
+                </Link>
 
-export default Sidebar;
+                <SidebarMenu />
+                <div className="divider"></div>
+            </DrawerSideWrapper>
+        </DrawerSidebar>
+    )
+}
+
+export default Sidebar
