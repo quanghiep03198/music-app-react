90528b2fa95de973d287ec390a9dd7d302e75bcf
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-24 01:35:41 +0700

    lazy loading app

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import store, { persistor } from "./app/redux/store";
+import ReactDOM from "react-dom/client"
+import { Provider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react"
+import store, { persistor } from "./app/redux/store"
 
-import { StrictMode } from "react";
-import App from "./App";
-import "./index.css";
-import ErrorBoundary from "./components/customs/ErrorBoundary";
+import { lazy, StrictMode, Suspense } from "react"
+const App = lazy(() => import("./App"))
+import "./index.css"
+import LoadingScreen from "./components/customs/LoadingScreen"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistor}>
-				<App />
-			</PersistGate>
-		</Provider>
-	</StrictMode>,
-);
+    <StrictMode>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Suspense fallback={<LoadingScreen />}>
+                    <App />
+                </Suspense>
+            </PersistGate>
+        </Provider>
+    </StrictMode>
+)
