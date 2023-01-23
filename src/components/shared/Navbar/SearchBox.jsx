fe9edf3d5649cf95260d336ba52d42992c5878d9
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-24 01:40:49 +0700

    refactor:  navigate to search page

--- src/components/shared/Navbar/SearchBox.jsx
+++ src/components/shared/Navbar/SearchBox.jsx
@@ -1,10 +1,11 @@
 import { useId } from "react"
 import { BsSearch } from "react-icons/bs"
+import { useNavigate } from "react-router-dom"
 import Button from "../../customs/Atomics/Button"
 
 const SearchBox = () => {
     const id = useId()
-
+    const navigate = useNavigate()
     return (
         <form className="flex items-center gap-1 rounded-full border border-neutral bg-base-200 px-2 ">
             <Button
@@ -21,6 +22,7 @@ const SearchBox = () => {
                 className={`input input-sm rounded-full bg-transparent focus:outline-none`}
                 id={id}
                 placeholder="Search ..."
+                onFocus={() => navigate("/search")}
             />
         </form>
     )
