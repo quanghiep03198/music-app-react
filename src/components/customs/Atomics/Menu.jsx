e22fabe40f49a0d4bdbbe4ec6864b9d411ff2101
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-23 14:51:29 +0700

    menu component

--- src/components/customs/Atomics/Menu.jsx
+++ src/components/customs/Atomics/Menu.jsx
@@ -1,19 +1,13 @@
-import React from "react";
-import tw from "tailwind-styled-components";
-export const MenuItem = ({ children, onClick }) => {
-	return (
-		<li onClick={onClick} className="truncate">
-			<a role="menuitem">{children}</a>
-		</li>
-	);
-};
-export const Menu = tw.ul`menu p-1 ${(props) => {
-	switch (props.direction) {
-		case "vertical":
-			return "vertical-menu";
-		case "horizontal":
-			return "horizontal-menu";
-		default:
-			return "vertical-menu";
-	}
-}}`;
+import React from "react"
+import tw from "tailwind-styled-components"
+export const MenuItem = tw.li`truncate`
+export const Menu = tw.ul`menu rounded-xl p-1 ${(props) => {
+    switch (props.direction) {
+        case "vertical":
+            return "vertical-menu"
+        case "horizontal":
+            return "horizontal-menu"
+        default:
+            return "vertical-menu"
+    }
+}}`
