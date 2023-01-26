72ee672d53376f98fafc6fc184a5ec7b80e5f552
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-27 02:06:44 +0700

    avatar component

--- src/components/customs/Atomics/Avatar.jsx
+++ src/components/customs/Atomics/Avatar.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React from "react"
 
-const Avatar = ({ imageUrl, isOnline, size }) => {
-	return (
-		<div className={isOnline ? "avatar online" : "avatar"}>
-			<div className={`rounded-full`} style={{ width: size * 0.25 + "rem", height: size * 0.25 + "rem" }}>
-				<img src={imageUrl} />
-			</div>
-		</div>
-	);
-};
+import tw from "tailwind-styled-components"
 
-export default Avatar;
+const Avatar = tw.div`avatar [&>img]:rounded-full [&>img]:object-cover ${(
+    props
+) => {
+    switch (props.size) {
+        case "sm":
+            return "[&>img]:w-10 [&>img]:h-10"
+        case "md":
+            return "[&>img]:w-12 [&>img]:h-12"
+        case "lg":
+            return "[&>img]:w-16 [&>img]:h-16"
+
+        default:
+            return "[&>img]:w-12 [&>img]:h-12"
+    }
+}}	${(props) => (props.online ? "online" : "")}`
+
+export default Avatar
