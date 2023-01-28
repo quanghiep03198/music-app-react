b84eb708e1c6434f004234ce4f5093f4fd5109e6
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-28 14:35:14 +0700

    remove react strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -9,13 +9,11 @@ import "./index.css"
 const App = lazy(() => import("./App"))
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <Suspense fallback={<LoadingScreen />}>
-                    <App />
-                </Suspense>
-            </PersistGate>
-        </Provider>
-    </StrictMode>
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <Suspense fallback={<LoadingScreen />}>
+                <App />
+            </Suspense>
+        </PersistGate>
+    </Provider>
 )
