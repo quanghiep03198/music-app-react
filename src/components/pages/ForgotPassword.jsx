ae0b2d646f858d29e0a91da756c3efb0f2eb76c2
Quang Hiệp <quanghiep03198@gmail.com>
2023-02-03 03:09:56 +0700

    feat: recover password

---
+++ src/components/pages/ForgotPassword.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useRef } from "react"
+import Logo from "/images/logo.png"
+import ErrorBoundary from "../customs/ErrorBoundary"
+import { useForm } from "react-hook-form"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
+import Button from "../customs/atoms/Button"
+import { useState } from "react"
+import { toast, ToastContainer } from "react-toastify"
+import { setCookie } from "@/utils/cookie"
+
+const ForgotPassword = () => {
+    const {
+        register,
+        formState: { errors },
+        handleSubmit
+    } = useForm()
+    const emailRef = useRef(null)
+    const navigate = useNavigate()
+    const { ref } = register("email")
+    useEffect(() => {
+        emailRef.current.scrollIntoView({ behavior: "smooth" })
+    })
+
+    const onSubmit = async (data) => {
+        try {
+            const response = await axios.post("/forgot-password", data)
+            if (response.status === 404) {
+                toast.error(response.message)
+                return
+            }
+            sessionStorage.setItem("token", response.token)
+            toast.info("Check your email to get verify code!")
+            navigate("/reset-password")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <div className="card glass w-full max-w-sm flex-shrink-0 shadow-2xl">
+                        <div className="card-body text-left">
+                            <img src={Logo} className=" max-w-full object-cover text-center" />
+
+                            <ErrorBoundary>
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Email</span>
+                                        </label>
+                                        <input
+                                            type="text"
+                                            placeholder="email"
+                                            className="input-bordered input"
+                                            {...register("email", {
+                                                required: "Provide an email!",
+                                                pattern: {
+                                                    value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                                                    message: "Email is invalid"
+                                                }
+                                            })}
+                                            ref={(e) => {
+                                                ref(e)
+                                                emailRef.current = e
+                                            }}
+                                        />
+
+                                        {errors.email && <small className="error-message">{errors.email?.message}</small>}
+                                    </div>
+
+                                    <div className="form-control mt-6">
+                                        <Button color="success" className="text-lg capitalize">
+                                            Continue
+                                        </Button>
+                                    </div>
+                                </form>
+                            </ErrorBoundary>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ForgotPassword
--- src/components/pages/ResetPassword.jsx
+++ src/components/pages/ResetPassword.jsx
@@ -1,7 +1,108 @@
-import React from "react";
+import React, { useEffect, useRef } from "react"
+import Logo from "/images/logo.png"
+import ErrorBoundary from "../customs/ErrorBoundary"
+import { useForm } from "react-hook-form"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
+import Button from "../customs/atoms/Button"
+import { useState } from "react"
+import { toast, ToastContainer } from "react-toastify"
+import { getCookie } from "@/utils/cookie"
 
 const ResetPassword = () => {
-	return <div>ResetPassword</div>;
-};
+    const {
+        register,
+        formState: { errors },
+        handleSubmit
+    } = useForm()
+    const verifyCodeRef = useRef(null)
+    const navigate = useNavigate()
+    const { ref } = register("verifyCode")
+    useEffect(() => {
+        verifyCodeRef.current.scrollIntoView({ behavior: "smooth" })
+    })
 
-export default ResetPassword;
+    const onSubmit = async (data) => {
+        try {
+            const token = sessionStorage.getItem("token")
+            const response = await axios({
+                url: "/reset-password",
+                method: "POST",
+                headers: {
+                    Authorization: "Bearer " + token
+                },
+                data: data
+            })
+            console.log(response)
+            if (response.status === 403) {
+                toast.error(response.message)
+                return
+            }
+            toast.success("Reset password successfully!")
+            navigate("/reset-password")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <div className="card glass w-full max-w-sm flex-shrink-0 shadow-2xl">
+                        <div className="card-body text-left">
+                            <img src={Logo} className=" max-w-full object-cover text-center" />
+
+                            <ErrorBoundary>
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Verify Code</span>
+                                        </label>
+                                        <input
+                                            type="text"
+                                            placeholder="email"
+                                            className="input-bordered input"
+                                            {...register("verifyCode", {
+                                                required: "Provide an verify code!"
+                                            })}
+                                            ref={(e) => {
+                                                ref(e)
+                                                verifyCodeRef.current = e
+                                            }}
+                                        />
+
+                                        {errors.verifyCode && <small className="error-message">{errors.verifyCode?.message}</small>}
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">New Password</span>
+                                        </label>
+                                        <input
+                                            type="password"
+                                            className="input-bordered input"
+                                            {...register("password", {
+                                                required: "Provide a password",
+                                                minLength: {
+                                                    value: 6,
+                                                    message: "Password must have at least 6 characters"
+                                                }
+                                            })}
+                                        />
+                                        {errors.password && <small className="error-message">{errors.password?.message}</small>}
+                                    </div>
+                                    <div className="form-control mt-6">
+                                        <Button color="success" className="text-lg capitalize">
+                                            reset password
+                                        </Button>
+                                    </div>
+                                </form>
+                            </ErrorBoundary>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ResetPassword
