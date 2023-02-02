5ea2b15c5c17401329c7fcebf798a84940f9002f
Quang Hiệp <quanghiep03198@gmail.com>
2023-02-03 03:10:10 +0700

    update collection service

--- src/app/services/collectionApi.js
+++ src/app/services/collectionApi.js
@@ -6,6 +6,7 @@ const collectionApi = createApi({
     baseQuery: axiosBaseQuery(),
     reducerPath: "collections",
     refetchOnReconnect: true,
+    refetchOnMountOrArgChange: true,
     endpoints(builder) {
         return {
             fetchTrackCollection: builder.query({
