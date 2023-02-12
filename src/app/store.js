29950e9f9539dbb6dfb93afc0e564877c0b73150
quanghiep03198 <97147601+PeoScouser98@users.noreply.github.com>
2023-02-12 07:55:48 +0700

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -14,6 +14,7 @@ import trackApi from "./services/trackApi"
 
 const persistConfig = {
     key: "root",
+    blacklist:["auth","authApi"]
     storage
 }
 const persistedReducer = persistReducer(persistConfig, rootReducer)
